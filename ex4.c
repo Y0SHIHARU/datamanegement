8423fd3ab3a82ebdfe62acc404f07ef8254ee03d
Y0SHIHARU <109262248+Y0SHIHARU@users.noreply.github.com>
2022-07-14 19:32:01 +0900

    Create ex4.c
    "HoT: Initial revision"

---
+++ ex4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+
+  srand(time(NULL));
+
+  int j = 0 , k = 0;
+  
+  printf("Tossing a coin ...\n");
+  for(int i = 0 ; i < 3 ; i++){
+    int num = rand()%2;
+
+    if(num == 0){
+      printf("Round %d: Heads\n" , i );
+      j++;
+    }
+    else{
+      printf("Round %d: Tails\n", i );
+      k++;
+    }
+  }
+
+  printf("Heads : %d, Tail : %d\n" , j , k );
+
+  return 0;
+}
